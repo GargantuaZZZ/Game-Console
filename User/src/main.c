c83548a4ed35844811ec107e8030fde3ae3ae5c5
Gargantua <2823886342@qq.com>
2025-09-15 18:06:59 +0800

    test

--- User/src/main.c
+++ User/src/main.c
@@ -4,6 +4,6 @@ int main(void) {
     SYSCFG_DL_init();
     while (1) {
         DL_GPIO_togglePins(GPIO_test_PORT, GPIO_test_PIN_0_PIN);
-        delay_cycles(32000000);
+        delay_cycles(16000000);
     }
 }
