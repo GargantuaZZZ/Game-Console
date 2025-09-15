4eb2166ce3d8cbb1d487740b3b3a3b15b197670d
Gargantua <2823886342@qq.com>
2025-09-15 18:03:55 +0800

    test

---
+++ User/src/main.c
@@ -0,0 +1,9 @@
+#include "inc/ti_msp_dl_config.h"
+
+int main(void) {
+    SYSCFG_DL_init();
+    while (1) {
+        DL_GPIO_togglePins(GPIO_test_PORT, GPIO_test_PIN_0_PIN);
+        delay_cycles(32000000);
+    }
+}
