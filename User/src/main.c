d24d0ad7ac2aaf087b559967ac539a33358dbfe0
GargantuaZZZ <2823886342@qq.com>
2025-09-18 16:27:31 +0800

    加入DAC DMA

--- User/src/main.c
+++ User/src/main.c
@@ -1,6 +1,7 @@
 #include "inc/ti_msp_dl_config.h"
 #include "inc/SPIflash.h"
 #include "inc/OLED.h"
+#include "inc/Alerts.h"
 
 #define SPIF_DEBUG SPIF_DEBUG_DISABLE
 
@@ -8,12 +9,19 @@
 #define KEY_RELEASED    true
 #define STABLE_CNT      20
 
+#define IDLE_BUF1       0
+#define FILL_BUF1       1
+#define IDLE_BUF2       2
+#define FILL_BUF2       3
+
 #define READ_KEY(n)     DL_GPIO_readPins(KEYs_KEY##n##_PORT, KEYs_KEY##n##_PIN)
 #define SET_LED(n)      DL_GPIO_setPins(LEDs_LED##n##_PORT, LEDs_LED##n##_PIN)
 #define CLEAR_LED(n)    DL_GPIO_clearPins(LEDs_LED##n##_PORT, LEDs_LED##n##_PIN)
 
-bool key_state[7],is_key_triggered[7];
-uint8_t key_stable_cnt[7];
+bool        key_state[7],is_key_triggered[7];
+uint8_t     key_stable_cnt[7];
+uint8_t     difficulty,buffer_state;
+uint16_t    buffer1[512],buffer2[512];
 
 int main(void) {
     SYSCFG_DL_init();
@@ -23,9 +31,35 @@ int main(void) {
         is_key_triggered[i] = false;
         key_stable_cnt[i] = 0;
     }
+    buffer_state = IDLE_BUF1;
 
     while (1) {
+        if (buffer_state == FILL_BUF1){
+            DL_DMA_setSrcAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)&buffer1[0]);
+            DL_DMA_setDestAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)&(DAC0 -> DATA0));
+            DL_DMA_setTransferSize(DMA, DMA_CH0_CHAN_ID, 512);
+            DL_DMA_enableChannel(DMA, DMA_CH0_CHAN_ID);
+            buffer_state = IDLE_BUF1;
+        }
+        else if (buffer_state == FILL_BUF2){
+            DL_DMA_setSrcAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)&buffer2[0]);
+            DL_DMA_setDestAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t)&(DAC0 -> DATA0));
+            DL_DMA_setTransferSize(DMA, DMA_CH0_CHAN_ID, 512);
+            DL_DMA_enableChannel(DMA, DMA_CH0_CHAN_ID);
+            buffer_state = IDLE_BUF2;
+        }
+    }
+}
 
+void DAC12_IRQHandler(){
+    switch (DAC12_INT_IRQN){
+        case DAC12_INST_DMA_TRIGGER:
+            if (buffer_state == IDLE_BUF1)
+                buffer_state == FILL_BUF2;
+            else if (buffer_state == IDLE_BUF2)
+                buffer_state == FILL_BUF1;
+        break;
+        default:break;
     }
 }
 
@@ -48,7 +82,7 @@ void TIMER_KEYs_INST_IRQHandler(){
                 }
             }
 
-            break;
+        break;
         default:break;
     }
 }
